cea3442a04ad676f6f7024e9dcb61c6b0715ea14
anton <anton@Slesarievs-MacBook-Pro.local>
2019-03-22 13:31:36 +0200

    initial commit

---
+++ precache-manifest.488c4ae90d363856350bdef9ba7cbcd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "441d95deaa6860504878",
+    "url": "/static/js/main.1a13ebc9.chunk.js"
+  },
+  {
+    "revision": "c92e26814522c129bed8",
+    "url": "/static/js/2.1801db15.chunk.js"
+  },
+  {
+    "revision": "441d95deaa6860504878",
+    "url": "/static/css/main.6ad3f62a.chunk.css"
+  },
+  {
+    "revision": "c92e26814522c129bed8",
+    "url": "/static/css/2.ab26b21b.chunk.css"
+  },
+  {
+    "revision": "6a4890c7f74db0398202f4645681fe3c",
+    "url": "/index.html"
+  }
+];
